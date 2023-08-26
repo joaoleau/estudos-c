00db32d1340a8df216a7e5a008a5ba89aaa79a12
Joao Leal <joaoo.lealc@gmail.com>
2023-08-25 22:10:39 -0300

    docs: Update study materials on dynamic stacks

--- Estrutura-de-Dados/dinamic-stack2.c
+++ Estrutura-de-Dados/dinamic-stack2.c
@@ -1,16 +1,67 @@
 #include <stdio.h>
 #include <malloc.h>
 
-typedef struct no{
+typedef struct no
+{
     int num;
     struct no *proximo;
-}No;
+} No; // repesenta um elemento
 
-typedef struct{
+typedef struct
+{
     No *topo;
-    int tam;
-}Pilha;
+} Pilha; // representa a pilha
 
-int main(){
+void iniciar(Pilha *pilha)
+{
+    pilha->topo = NULL;
+}
+
+void push(Pilha *pilha, int value)
+{
+    No *novo = malloc(sizeof(No));
+    if (novo)
+    {
+        novo->num = value;
+        novo->proximo = pilha->topo;
+        pilha->topo = novo;
+    }
+    else
+    {
+        printf("Erro ao alocar");
+    }
+}
+
+void pop(Pilha *pilha)
+{
+    if (pilha->topo != NULL)
+    {
+        No *remover = pilha->topo;
+        pilha->topo = remover->proximo;
+        free(remover);
+    }
+    else
+    {
+        printf("Pilha ja esta vazia");
+    }
+}
+
+void view(Pilha *pilha)
+{
+    No *aux = pilha->topo;
+    while (aux)
+    {
+        printf("%d ", aux->num);
+        aux = aux->proximo;
+    }
+}
+
+int main()
+{
+    Pilha pilha;
+    iniciar(&pilha);
+    push(&pilha, 3);
+    push(&pilha, 13);
+    view(&pilha);
     return 0;
 }
